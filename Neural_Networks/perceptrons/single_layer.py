318c9c381b3e5da1164389688bc204b1d1a2d921
AndreMacedo88 <andre.lopes.macedo@gmail.com>
2024-05-09 12:22:07 +0200

    feat: :sparkles: implement an ADALINE (with logistic activation funtion for now...)

--- Neural_Networks/perceptrons/single_layer.py
+++ Neural_Networks/perceptrons/single_layer.py
@@ -32,3 +32,30 @@ class Perceptron(object):
 
     def predict(self, X):
         return act.step(x=self.net_input(X), threshold=0)
+
+
+class Adaline(object):
+    def __init__(self, eta=0.01, epochs=50):
+        self.eta = eta
+        self.epochs = epochs
+
+    def fit(self, X, y):
+
+        self.w_ = np.zeros(1 + X.shape[1])
+        self.errors_ = []
+
+        for _ in range(self.epochs):
+            errors = 0
+            for xi, target in zip(X, y):
+                update = self.eta * (target - self.predict(xi))
+                self.w_[1:] += update * xi
+                self.w_[0] += update
+                errors += int(update != 0.0)
+            self.errors_.append(errors)
+        return self
+
+    def net_input(self, X):
+        return (np.dot(X, self.w_[1:]) + self.w_[0]).flatten()
+
+    def predict(self, X):
+        return act.logistic(x=self.net_input(X))
