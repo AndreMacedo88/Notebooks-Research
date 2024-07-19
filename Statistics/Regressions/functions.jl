5cba8ab448be3a2c190bc476bb3f3f79eae86b16
AndreMacedo88 <andre.lopes.macedo@gmail.com>
2024-07-19 22:53:25 +0200

    feat: :alembic: implement OLS to estimate beta0 and beta1

--- Statistics/Regressions/functions.jl
+++ Statistics/Regressions/functions.jl
@@ -10,5 +10,10 @@ function OLS_estimation(x::Vector{<:Real}, y::Vector{<:Real})
 
     cov = ((1/n) .* sum(y.*x)) - ((1/(n^2)) .* sum(y) .* sum(x))
     var_x = ((1/n) .* sum(x.^2)) - (((1/n) .* sum(x))^2)
-    @printf("Covariance of x and y: %s \nVariance of x: %s", cov, var_x)
+    @printf("Covariance of x and y: %s \nVariance of x: %s\n", cov, var_x)
+
+    β1 = cov / var_x
+    β0 = ȳ - (β1 * x̄)
+    @printf("β0: %s \nβ1: %s\n", β0, β1)
+
 end
